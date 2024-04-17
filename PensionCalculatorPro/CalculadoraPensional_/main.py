6364b5c66990204fe7a098e8e8c1b0298cd51bb2
Nataliafg7 <127059274+Nataliafg7@users.noreply.github.com>
2024-04-16 22:14:35 -0500

    Delete PensionCalculatorPro/CalculadoraPensional_ directory

--- PensionCalculatorPro/CalculadoraPensional_/main.py
+++
@@ -1,6 +0,0 @@
-from kivy.app import App
-
-# Importa la clase PensionCalculatorApp desde el archivo pension_interface.py
-from src.Gui.pension_interface import *
-if __name__ == '__main__':
-    PensionCalculatorApp().run()
