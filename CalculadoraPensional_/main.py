3f50894f134c9dc163d2c392ce423edc077039f1
Nataliafg7 <127059274+Nataliafg7@users.noreply.github.com>
2024-04-16 22:16:10 -0500

    Add files via upload

---
+++ CalculadoraPensional_/main.py
@@ -0,0 +1,6 @@
+from kivy.app import App
+
+# Importa la clase PensionCalculatorApp desde el archivo pension_interface.py
+from src.Gui.pension_interface import *
+if __name__ == '__main__':
+    PensionCalculatorApp().run()
